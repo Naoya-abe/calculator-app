13650b250dd1d3e41a8bb6baa7a62f31eb9b2b88
阿部直也 <n.abe@gemcook.com>
2020-10-10 10:25:38 +0900

    complited App

---
+++ src/redux/actions/types.js
@@ -0,0 +1,14 @@
+// 『数字』が押された時
+export const INPUT_NUMBER = 'INPUT_NUMBER';
+
+// 『+、-、×、÷』が押された時
+export const PLUS = 'PLUS';
+export const MINUS = 'MINUS';
+export const MULTIPLY = 'MULTIPLY';
+export const DIVIDE = 'DIVIDE';
+
+// 『=』が押された時
+export const EQUAL = 'EQUAL';
+
+// 『AC』が押された時
+export const CLEAR = 'CLEAR';
